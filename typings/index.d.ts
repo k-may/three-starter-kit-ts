9e1d7a9711bf36dadb292cbfba6ddb436053052b
k-may <kevmayo@gmail.com>
2017-02-21 12:01:17 +0100

    amd resolution

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/three/index.d.ts" />
-/// <reference path="modules/debug/index.d.ts" />
-/// <reference path="modules/requirejs/index.d.ts" />
